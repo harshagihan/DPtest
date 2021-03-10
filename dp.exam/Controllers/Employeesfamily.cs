d16df31028e74d26339175ad08f65e406dddcf61
Harsha Gihan Madhushankha Liyanage <57912882+harshagihan@users.noreply.github.com>
2021-03-10 14:44:12 +0530

    Update Employeesfamily.cs

--- dp.exam/Controllers/Employeesfamily.cs
+++ dp.exam/Controllers/Employeesfamily.cs
@@ -53,8 +53,7 @@ namespace dp.exam.Controllers
         }
 
         // POST: Employeesfamily/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("FristName,Surname,Relationship,FkEmployeeId,Id")] EmployeeFamily employeeFamily)
@@ -87,8 +86,7 @@ namespace dp.exam.Controllers
         }
 
         // POST: Employeesfamily/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+      
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("FristName,Surname,Relationship,FkEmployeeId,Id")] EmployeeFamily employeeFamily)
